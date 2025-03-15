ded00d0d992794867a6257dbae02eca2cc9527ce
= <=>
2025-03-15 18:28:29 -0300

    add generate function

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    generated_password = "".join(password_list)
+    if len(password_entry.get()) == 0:
+        password_entry.insert(0, generated_password)
+    else:
+        password_entry.delete(0, END)
+        password_entry.insert(0, generated_password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
@@ -51,7 +70,7 @@ password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1, columnspan=2)
 
 # Buttons
-generate_password_button = Button(text="Generate")
+generate_password_button = Button(text="Generate", command=generate_password)
 generate_password_button.grid(row=3, column=3)
 add_button = Button(text="Add", width=25,height=2, command=save)
 add_button.grid(row=4, column=1, columnspan=2, pady=20)
