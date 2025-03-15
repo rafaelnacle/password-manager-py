4c6fead90206599ff17ade459af1efe640e78ec1
= <=>
2025-03-15 17:54:03 -0300

    Add password to file

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -25,9 +32,11 @@ password_label.grid(row=3, column=0)
 # Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "rafael@mail.com")
 
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
@@ -35,7 +44,7 @@ password_entry.grid(row=3, column=1)
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
