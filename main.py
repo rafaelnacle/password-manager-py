b5c07de52a2445c694daebd70b8786afd2ecbfa7
= <=>
2025-03-16 13:45:52 -0300

    treat errors and exceptions

--- main.py
+++ main.py
@@ -45,16 +45,21 @@ def save():
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
         if is_ok:
-            with open("data.json", "r") as data_file:
-                # Reading old data
-                data = json.load(data_file)
-                # Updating old data with new data
+            try:
+                with open("data.json", "r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                 # Updating old data with new data
                 data.update(new_data)
 
-            with open("data.json", "w") as data_file:
-                # Saving updated data
-                json.dump(data, data_file, indent=4)
-
+                with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+            finally:
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
